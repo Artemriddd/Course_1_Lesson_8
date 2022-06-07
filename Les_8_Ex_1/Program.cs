37b4856cca614699775b238ae0469419ac30e30f
Artem <Artemrid@yandex.ru>
2022-06-07 20:04:04 +0300

    HomeWork #8

---
+++ Les_8_Ex_1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Les_8_Ex_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Properties.Settings.Default.Hello);
+            if (string.IsNullOrEmpty(Properties.Settings.Default.Fio))
+            {
+                Console.Write("Введите имя пользователя: ");
+                Properties.Settings.Default.Fio = Console.ReadLine();
+                Console.Write("Введите Возраст: ");
+                Properties.Settings.Default.Age = Int32.Parse(Console.ReadLine());
+                Console.Write("Введите профессию: ");
+                Properties.Settings.Default.Profession = Console.ReadLine();
+                Properties.Settings.Default.Save();
+            }
+            else
+            {
+                Console.Write("Имя пользователя: ");
+                Console.WriteLine(Properties.Settings.Default.Fio);
+                Console.Write("Возраст: ");
+                Console.WriteLine(Properties.Settings.Default.Age);
+                Console.Write("Профессия: ");
+                Console.WriteLine(Properties.Settings.Default.Profession);
+            }
+            Console.ReadKey(true);
+        }
+    }
+}
---
+++ Les_8_Ex_1/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("Les_8_Ex_1")]
+[assembly: AssemblyDescription("HomeWork #8")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Irkut Corporation")]
+[assembly: AssemblyProduct("Les_8_Ex_1")]
+[assembly: AssemblyCopyright("Copyright © Irkut Corporation 2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("99a7fa9e-a23d-44c0-a205-d3c296001f23")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
